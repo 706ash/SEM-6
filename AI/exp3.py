dab210f164f71d7c93c2e13fc0f47c4ede8aa0aa
706ash <carameloco7@gmail.com>
2025-02-04 01:17:41 +0530

    Update exp3.py

---
+++ AI/exp3.py
@@ -0,0 +1,66 @@
+def movegen(current, graph):
+    return [[n, current] for n in graph[current]]
+
+
+def goal_test(current, goal):   
+    return current in goal
+
+
+def traversal(closed):
+    print("Traversal ", end="")
+
+    for i in range(len(closed)):
+        print(closed[i][0], end=" ")
+        if i != len(closed) - 1: 
+            print("-->", end=" ")
+  
+    
+def dfs(graph):
+    open = []
+    closed = []
+    
+    start_node = input("\n\nEnter the start node: ")
+    goal_node = input("Enter the goal node or nodes seperated by spaces: ").split()
+    
+    open.append([start_node,None])
+    
+    while open:
+        current = open[0][0]
+        closed.append(open[0])
+        del open[0]
+        if goal_test(current, goal_node):
+            print("\n\nGoal found\n")
+            traversal(closed)
+            return                    
+        else:
+            child_list = movegen(current, graph)
+            for child in child_list:
+                if any(child[0] == o[0] for o in open) or any(child[0] == c[0] for c in closed):
+                    continue
+                open.insert(0, child)
+            
+            
+    
+    if not goal_node:
+        print("\n\nGoal node not specified")
+        traversal(closed)
+    else:print("\n\nGoal not found")
+            
+            
+def input_graph():
+    graph = {}
+
+    n = int(input("Enter the number of nodes: "))
+    
+    for i in range(n):
+        node = input("Enter a node: ")
+        neighbours = input(f"Enter the neighbours of {node} seperated by spaces: ").split()
+        graph[node] = neighbours
+
+    return graph
+
+
+if __name__ == "__main__":
+    graph = input_graph()
+    dfs(graph)
+    print()
